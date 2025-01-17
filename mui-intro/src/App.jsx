7263c8f1ed1337190705e9fd6ef2210411eaad9d
Suyasha Nepal <nepalsuyasha@gmail.com>
2025-01-17 11:37:07 +0545

    MUI intro#

--- mui-intro/src/App.jsx
+++ mui-intro/src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import Actors from './components/component/Actors';
+import Animal from './components/component/Animal';
+import Book from './components/component/Book';
+import Counter from './components/component/Counter';
 import CustomButton from './components/button/CustomButton';
 import CustomTypography from './components/paragraph/CustomTypography';
-import Counter from './components/counter/Counter';
-import Person from './components/counter/Person';
-import Animal from './components/counter/Animal';
-import Student from './components/counter/Student';
-import Actors from './components/counter/Actors';
-import Book from './components/counter/Book';
-
+import Person from './components/component/Person';
+import React from 'react';
+import Student from './components/component/Student';
 const App = () => {
   return (
     <div>
@@ -22,5 +21,9 @@ const App = () => {
     </div>
   );
 };
+
+// .jsx file= JS + HTML
+// browser doesn't process .jsx, so the above conversion must be done
+// this conversion is done by a transpiler called babel in react
 
 export default App;
